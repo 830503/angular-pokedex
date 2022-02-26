7f12e747d1ed74095e1d805ec2e66824f1ee64a3
830503 <lixiaoqi19830503@gmail.com>
2022-02-26 08:21:03 +0100

    added pokemonList and searchbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PokemonListComponent,
+    SearchBarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgxPaginationModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../service/data.service';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.css'],
+})
+export class PokemonListComponent implements OnInit {
+  pokemons: any[] = [];
+  page: number = 1;
+  totalPages: number;
+  offset: number;
+  limit: number = 8;
+
+  searchPoke: any[] = [];
+  isSearching: boolean = false;
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.getPokemons();
+  }
+
+  //Get pokemons
+  getPokemons() {
+    this.offset = this.page * this.limit - this.limit;
+    this.dataService
+      .getPokemons(this.limit, this.offset)
+      .subscribe((response: any) => {
+        // console.log(response);
+        this.totalPages = response.count;
+
+        response.results.forEach((result: { name: string }) => {
+          this.dataService
+            .getMoreData(result.name)
+            .subscribe((uniqueResponse: any) => {
+              this.pokemons.push(uniqueResponse);
+              this.pokemons.sort((a, b) => (a.id > b.id ? 1 : -1));
+              console.log(this.pokemons);
+            });
+        });
+      });
+  }
+  // the searchbar works not well
+  // getSearch(value: string) {
+  //   const filter = this.pokemons.filter((res: any) => {
+  //     return !res.name.indexOf(value.toLocaleLowerCase());
+  //   });
+  //   this.pokemons = filter;
+  // }
+  getSearch(value: string) {
+    this.offset = 100;
+    this.dataService
+      .getPokemons(this.limit, this.offset)
+      .subscribe((response: any) => {
+        // console.log(response);
+        this.totalPages = response.count;
+
+        response.results.forEach((result: { name: string }) => {
+          this.dataService
+            .getMoreData(result.name)
+            .subscribe((uniqueResponse: any) => {
+              this.pokemons.push(uniqueResponse);
+              this.pokemons.sort((a, b) => (a.id > b.id ? 1 : -1));
+              console.log(this.pokemons);
+            });
+        });
+      });
+  }
+}
