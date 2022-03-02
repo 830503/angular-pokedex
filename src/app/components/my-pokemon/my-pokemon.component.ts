9f55d48ac1d4cc7281696ceb8f735e4459dbb1ab
830503 <lixiaoqi19830503@gmail.com>
2022-03-02 11:44:40 +0100

    style my-poke page and added detail button

--- src/app/components/my-pokemon/my-pokemon.component.ts
+++ src/app/components/my-pokemon/my-pokemon.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MyPokemonService } from '../../service/my-pokemon.service';
+import { PokemonDetailsComponent } from '../pokemon-details/pokemon-details.component';
+import { PokemonDetails } from 'src/app/model/poke-detail.model';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
 
 @Component({
   selector: 'app-my-pokemon',
@@ -7,10 +10,17 @@ import { MyPokemonService } from '../../service/my-pokemon.service';
   styleUrls: ['./my-pokemon.component.css'],
 })
 export class MyPokemonComponent implements OnInit {
-  @Input() myPokes: any[] = [];
+  myPokes: PokemonDetails[] = [];
 
-  constructor(private myPokemon: MyPokemonService) {
+  constructor(
+    private myPokemon: MyPokemonService,
+    private bottomSheet: MatBottomSheet
+  ) {
     this.myPokes = this.myPokemon.myPokemons;
+  }
+
+  onDetail(pokemon: PokemonDetails): void {
+    this.bottomSheet.open(PokemonDetailsComponent, { data: { pokemon } });
   }
 
   ngOnInit(): void {}
--- src/app/model/poke-detail.model.ts
+++ src/app/model/poke-detail.model.ts
@@ -3,31 +3,49 @@ export class PokemonDetails {
   name: string;
   height: number;
   weight: number;
-  abilities: any[];
-  types: any[];
-  moves: any[];
+  types: any;
+  moves: any;
   sprites: any;
-  stats: any[];
+  stats: any;
 
   constructor(
     id: number,
     name: string,
     height: number,
     weight: number,
-    abilities: any[],
-    types: any[],
-    moves: any[],
-    sprites: any,
-    stats: any[]
+    types: Type[],
+    moves: Move[],
+    sprites: Sprite,
+    stats: Stat[]
   ) {
     this.id = id;
     this.name = name;
     this.height = height;
     this.weight = weight;
-    this.abilities = abilities;
     this.types = types;
     this.moves = moves;
     this.sprites = sprites;
     this.stats = stats;
   }
+}
+
+class Type {
+  type?: {
+    name: string;
+  };
+}
+
+class Sprite {
+  front_default?: string;
+}
+
+class Move {
+  move?: {
+    name: string;
+  };
+}
+class Stat {
+  stat?: {
+    name: string;
+  };
 }
